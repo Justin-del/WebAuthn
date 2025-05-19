733fe45e6c7c156479e3ebd2f4518be6a33532f8
Justin-del <beibei031119@gmail.com>
2025-05-19 18:07:18 +0800

    commit

--- WebAuthn/WebAuthn.go
+++ WebAuthn/WebAuthn.go
@@ -177,7 +177,7 @@ func RegisterPublicKeyCredential(session *Session, publicKeyCredential *Registra
 
 	fmt.Println("Is client data json correct", isClientDataJSONCorrect)
 	fmt.Println("Is correct hash", IsCorrectHash([32]byte(attestationObject.AuthData[0:32])))
-	fmt.Println("Is valid key", credentialPublicKeyMap)
+	fmt.Println("Is valid key", IsValidKey(credentialPublicKeyMap))
 	fmt.Println(AreFlagsValid(attestationObject.AuthData[32], session.UserVerification == "required"))
 
 	canRegister := isClientDataJSONCorrect && IsCorrectHash([32]byte(attestationObject.AuthData[0:32])) && AreFlagsValid(attestationObject.AuthData[32], session.UserVerification == "required") && IsValidKey(credentialPublicKeyMap)
