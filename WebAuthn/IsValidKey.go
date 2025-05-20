3c685981f59ea7fde1ac123045a37fbd97d99847
Justin-del <beibei031119@gmail.com>
2025-05-20 19:24:42 +0800

    commit

--- WebAuthn/IsValidKey.go
+++ WebAuthn/IsValidKey.go
@@ -1,5 +1,7 @@
 package webauthn
 
+import "fmt"
+
 func isValidES256Key(publicKey map[int64]any) bool {
 	return publicKey[1].(uint64) == 2 && publicKey[3].(int64) == -7 && publicKey[-1].(uint64) == 1
 }
@@ -13,5 +15,6 @@ func isValidRS256Key(publicKey map[int64]any) bool {
 }
 
 func IsValidKey(publicKey map[int64]any) bool {
+	fmt.Println(publicKey)
 	return (publicKey[-2] != nil && publicKey[-3] != nil) && (isValidES256Key(publicKey) || isValidEdDsaKey(publicKey) || isValidRS256Key(publicKey))
 }
